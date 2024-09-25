037d599af980a534ae545b111da94191578a30fe
Hugo <hoyola@ucv.edu.pe>
2024-09-25 12:50:29 -0500

    Refactorizar componentes y estilos
    - Refactorizar el componente app.component.html para agregar una sección con una tarjeta de material y una barra de herramientas.
    - Agregar estilos globales en app.component.scss para establecer la fuente, el margen, el relleno, el tamaño mínimo y el color de fondo.
    - Agregar nuevos componentes: filter, grid, modal, toolbar y list.
    - Configurar las importaciones necesarias en app.component.ts y en los nuevos componentes.
    - Agregar archivos de plantilla y estilos para los componentes grid, modal y toolbar.
    - Agregar archivo de prueba para los componentes filter, grid, modal, toolbar y list.
    - Actualizar el archivo tsconfig.json para incluir rutas de importación para los componentes y los archivos de entorno.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+import { ToolbarComponent } from '@components/toolbar/toolbar.component';
+
+const MATERIAL_MODULES = [MatCardModule];
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MATERIAL_MODULES, ToolbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'contacts-firebase-angular';

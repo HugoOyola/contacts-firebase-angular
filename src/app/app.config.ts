2e7b9a9c9692ba728bbde7ad803a4eb128b6f9f0
Hugo <hoyola@ucv.edu.pe>
2024-09-25 10:34:52 -0500

    Agregar configuración de Firebase y Angular Fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,14 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '@env/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"app-contacts-e57e1","appId":"1:204800777813:web:d7bb972a16e0c0e8513edc","storageBucket":"app-contacts-e57e1.appspot.com","apiKey":"AIzaSyCnQcMeIy_JSePZ49LFDJGZiwm6bt2f4Uw","authDomain":"app-contacts-e57e1.firebaseapp.com","messagingSenderId":"204800777813"})), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'app-contacts-e57e1',
+    appId: '1:204800777813:web:d7bb972a16e0c0e8513edc',
+    storageBucket: 'app-contacts-e57e1.appspot.com',
+    apiKey: 'AIzaSyCnQcMeIy_JSePZ49LFDJGZiwm6bt2f4Uw',
+    authDomain: 'app-contacts-e57e1.firebaseapp.com',
+    messagingSenderId: '204800777813',
+  },
+};

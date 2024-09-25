3a60c98a41905e5cbdb13f13d6a4db28894f4f19
Hugo Oyola <hugo.ham@outlook.com>
2024-09-24 21:40:09 -0500

    Agregar configuración de Firebase y Angular Fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"app-contacts-e57e1","appId":"1:204800777813:web:d7bb972a16e0c0e8513edc","storageBucket":"app-contacts-e57e1.appspot.com","apiKey":"AIzaSyCnQcMeIy_JSePZ49LFDJGZiwm6bt2f4Uw","authDomain":"app-contacts-e57e1.firebaseapp.com","messagingSenderId":"204800777813"})), provideFirestore(() => getFirestore())]
 };
